342c0cf5ba15e26d8e91cb09208736b701c94573
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-09-17 14:44:30 +0200

    feat: add scheme validation (#3)

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,9 @@
 import { ObjectId } from 'bson'
+import { z, ZodIssue, ZodType } from 'zod'
+import {
+  Filter as MongoDBFilter,
+  UpdateFilter as MongoDBUpdateFilter,
+} from 'mongodb'
 
 export type Document = {
   _id: ObjectId
@@ -6,7 +11,31 @@ export type Document = {
   updatedAt: string
 }
 
-export type Collection<T extends Record<string, unknown>> =
-  globalThis.Realm.Services.MongoDB.MongoDBCollection<T & Document>
+export type Collection<T extends Record<string, ZodType>> =
+  globalThis.Realm.Services.MongoDB.MongoDBCollection<Infer<T> & Document>
 
 export type Database = ReturnType<globalThis.Realm.Services.MongoDB['db']>
+
+export type ErrorListener = (
+  event:
+    | {
+      type: 'ValidationError'
+      issues: ZodIssue[]
+      message: string
+    }
+    | {
+      type: 'InitializationError'
+      message: string
+    },
+) => unknown
+
+export type Infer<T extends Record<string, ZodType>> = z.infer<
+  ReturnType<typeof z.object<T>>
+>
+
+export type Filter<T extends Record<string, ZodType>> = MongoDBFilter<
+  Infer<T> & { createdAt: string; updatedAt: string }
+>
+
+export type UpdateFilter<T extends Record<string, ZodType>> =
+  MongoDBUpdateFilter<Infer<T> & { createdAt: string; updatedAt: string }>
